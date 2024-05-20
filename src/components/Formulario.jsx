9e6c746f105839033095627e06d5449eba6887d2
XimenaFerrer28 <150406879+XimenaFerrer28@users.noreply.github.com>
2024-05-19 20:32:03 -0600

    Nuevos parámetros

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -6,51 +6,77 @@ const Formulario = () => {
   const { guardarCancion, cancion } = useCanciones();
   const [nombre, setNombre] = useState("");
   const [album, setAlbum] = useState("");
-  const [ano_lanzamiento, setLanzamiento] = useState("");
+  const [año_lanzamiento, setLanzamiento] = useState("");
   const [duracion, setDuracion] = useState("");
   const [compositor, setCompositor] = useState("");
   const [productor, setProductor] = useState("");
   const [email, setEmail] = useState("");
-  const [id_cancion, setId] = useState(null)
+  const [url, setUrl] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+
+  const [id_cancion, setId] = useState(null);
+  // Implementacion de la alerta
+  const [alerta, setAlerta] = useState({});
 
   useEffect(() => {
     if (cancion.id_cancion !== "") {
         setNombre(cancion.nombre || "");
         setAlbum(cancion.album || "");
-        setLanzamiento(cancion.ano_lanzamiento || "");
+        setLanzamiento(cancion.año_lanzamiento || "");
         setDuracion(cancion.duracion || "");
         setCompositor(cancion.compositor || "");
         setProductor(cancion.productor || "");
         setEmail(cancion.email || "");
+        setUrl(cancion.url || "");
+        setDescripcion(cancion.descripcion || "");
+
         setId(cancion.id_cancion || "");
-        // No actualices el estado de email aquí
     }
   }, [cancion]);
   
   
   
   
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
     // Crear un objeto con los datos del formulario
-    const nuevoCancion = {
+    if([nombre, album, duracion, compositor, productor, email, año_lanzamiento, url, descripcion].includes('')){
+      
+      
+      
+      setAlerta({
+        msg: "Todos los campos son obligatorios",
+        error: true
+      })
+
+      setTimeout(() => {
+        setAlerta(false);
+      }, 5000);
+      return;
+
+
+
+    }
+    // Enviar los datos al backend utilizando la función guardarProducto del hook
+    guardarCancion({
       nombre,
       album,
-      ano_lanzamiento,
+      año_lanzamiento: new Date(año_lanzamiento).toISOString(),
       duracion,
       compositor,
       productor,
       email,
+      url, 
+      descripcion,
       id_cancion
-    };
-
-    // Enviar los datos al backend utilizando la función guardarProducto del hook
-    await guardarCancion(nuevoCancion);
-
-    console.log(nuevoCancion);
+    });
 
     // Limpiar el formulario después de enviar los datos
+    
+    // Función para mostrar la alerta
+    
+    
     setNombre("");
     setCompositor("");
     setAlbum("");
@@ -58,26 +84,32 @@ const Formulario = () => {
     setLanzamiento("");
     setProductor("");
     setEmail("");
+    setDescripcion("");
+    setUrl("");
     setId("")
+
   };
+ 
+
+  const {msg} = alerta
   
   return (
     <>
-      <h2 className="font-black text-3xl text-center">
+      <h2 className="font-black text-4xl text-center font-enca">
         Administrador de Canciones
       </h2>
 
-      <p className="text-xl mt-5 mb-10 text-center">
-        Añade tus Canciones Favoritas y {""}
-        <span className="text-teal-600 font-bold">Administralos</span>
+      <p className="text-3xl text-white mt-5 mb-10 text-center font-formu">
+      Añade tus Canciones  {""}
+        <span className="text-white font-bold">Favoritas</span>
       </p>
 
       <form
-        className="bg-gray-500 py-10 px-5 mb-10 lg:mb-5 shadow-md rounded-md"
+        className="bg-gray-500 py-10 px-5 mb-10 lg:mb-5 shadow-md rounded-md font-formu"
         onSubmit={handleSubmit}
       >
         <div className="mb-5">
-          <label htmlFor="nombre" className="text-gray-700 uppercase font-bold">
+          <label htmlFor="nombre" className="text-gray-950 uppercase font-bold  text-xl">
             Nombre
           </label>
           <input
@@ -91,7 +123,7 @@ const Formulario = () => {
         </div>
 
         <div className="mb-5">
-          <label htmlFor="number" className="text-gray-700 uppercase font-bold">
+          <label htmlFor="album" className="text-gray-950 uppercase font-bold  text-xl">
           album
           </label>
           <input
@@ -104,7 +136,7 @@ const Formulario = () => {
           />
         </div>
         <div className="mb-5">
-          <label htmlFor="number" className="text-gray-700 uppercase font-bold">
+          <label htmlFor="compositor" className="text-gray-950 uppercase font-bold  text-xl">
           Compositor
           </label>
           <input
@@ -119,22 +151,22 @@ const Formulario = () => {
 
         <div className="mb-5">
           <label
-            htmlFor="categoria"
-            className="text-gray-700 uppercase font-bold"
+            htmlFor="año_lanzamiento"
+            className="text-gray-950 uppercase font-bold  text-xl"
           >
             Año de lanzamiento
           </label>
           <input
-            id="ano_lanzamiento"
+            id="año_lanzamiento"
             type="date"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-            value={ano_lanzamiento}
+            value={año_lanzamiento}
             onChange={e => setLanzamiento(e.target.value)}
           />
         </div>
 
         <div className="mb-5">
-          <label htmlFor="piezas" className="text-gray-700 uppercase font-bold">
+          <label htmlFor="duracion" className="text-gray-950 uppercase font-bold  text-xl">
           Duracion
           </label>
           <input
@@ -150,13 +182,14 @@ const Formulario = () => {
         <div className="mb-5">
           <label
             htmlFor="categoria"
-            className="text-gray-700 uppercase font-bold"
+            className="text-gray-950 uppercase font-bold  text-xl"
           >
             Productor
           </label>
           <input
             id="Productor"
             type="text"
+            placeholder="Productor"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             value={productor}
             onChange={e => setProductor(e.target.value)}
@@ -164,27 +197,58 @@ const Formulario = () => {
         </div>
         
         <div className="mb-5">
-          <label
-            htmlFor="descripcion"
-            className="text-gray-700 uppercase font-bold"
-          >
+          <label htmlFor="email" className="text-gray-950 uppercase font-bold  text-xl">
             Email
           </label>
-          <textarea
+          <input
             id="email"
-            placeholder="Email@emil.com"
+            type="text"
+            placeholder="Email@email.com"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             value={email}
             onChange={e => setEmail(e.target.value)}
           />
         </div>
 
+
+        <div className="mb-5">
+          <label htmlFor="url" className="text-gray-950 uppercase font-bold  text-xl">
+            Url
+          </label>
+          <input
+            id="url"
+            type="text"
+            placeholder="https//..."
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={url}
+            onChange={e => setUrl(e.target.value)}
+          />
+        </div>
+
+
+        <div className="mb-5">
+          <label htmlFor="descripcion" 
+                className="text-gray-950 uppercase font-bold  text-xl">
+            Descripción 
+          </label>
+          <textarea
+            id="Descripción"
+            type="text"
+            placeholder="Descripción"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={descripcion}
+            onChange={e => setDescripcion(e.target.value)}
+          />
+        </div>
+
+
         <input
           type="submit"
-          className="bg-teal-600 w-full p-3 text-white uppercase font-bold hover:bg-teal-700 cursor-pointer transition-colors"
-          value={cancion.id_cancion ? 'Guardar Cambios' : 'Agregar Producto'}
+          className="bg-[#202355] w-full p-3 text-white uppercase font-bold hover:bg-[#15173a] cursor-pointer transition-colors  text-2xl"
+          value={cancion.id_cancion ? 'Guardar Cambios' : 'Agregar Canción'}
         />
       </form>
+      {msg && <Alerta alerta={alerta} />}
     </>
   );
 };
